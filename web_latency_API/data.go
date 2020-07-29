5d7265d9fea0deee48188aa150b671b236abbb17
excircle <unixdisiple.us@gmail.com>
2020-07-29 11:59:03 -0700

    Adding web_latency source files

---
+++ web_latency_API/data.go
@@ -0,0 +1,26 @@
+package mirrors
+
+// MirrorList is list of Debian mirror sites
+var MirrorList = []string {
+	"http://ftp.am.debian.org/debian/",
+	"http://ftp.au.debian.org/debian/",
+	"http://ftp.at.debian.org/debian/",
+	"http://ftp.by.debian.org/debian/",
+	"http://ftp.be.debian.org/debian/",
+	"http://ftp.br.debian.org/debian/",
+	"http://ftp.bg.debian.org/debian/",
+	"http://ftp.ca.debian.org/debian/",
+	"http://ftp.cl.debian.org/debian/",
+	"http://ftp2.cn.debian.org/debian/",
+	"http://ftp.cn.debian.org/debian/",
+	"http://ftp.hr.debian.org/debian/",
+	"http://ftp.cz.debian.org/debian/",
+	"http://ftp.dk.debian.org/debian/",
+	"http://ftp.sv.debian.org/debian/",
+	"http://ftp.ee.debian.org/debian/",
+	"http://ftp.fr.debian.org/debian/",
+	"http://ftp2.de.debian.org/debian/",
+	"http://ftp.de.debian.org/debian/",
+	"http://ftp.gr.debian.org/debian/",
+}
+
